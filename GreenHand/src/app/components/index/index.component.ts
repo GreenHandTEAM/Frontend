e63dbc208cd7d03ad1e9e86ab45806ebceef3d1b
jangel347 <74575479+jangel347@users.noreply.github.com>
2021-11-27 11:58:08 -0500

    FUNCIONALIDAD: Filtros y estilos
    - Se agrega filtro en el index
    - Se agregan estilos a las tarjetas
    - Se agregan estilos al nav bar y se cambia la estructura

--- GreenHand/src/app/components/index/index.component.ts
+++ GreenHand/src/app/components/index/index.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-index',
@@ -9,11 +11,28 @@ import { ProductService } from 'src/app/services/product.service';
 export class IndexComponent implements OnInit {
 
   productSet = [];
+  productFil: Product[];
+  searchTerm: string;
+  faCheckCircle = faCheckCircle;
+
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productSet = [];
     this.refreshList();
+  }
+
+  search(value: string): void {
+    for (let i = 0; i < this.productSet.length; i++) {
+      const ELEMENT = document.getElementById("card" + this.productSet[i].id);
+      const CONTENT = ELEMENT.innerText.toLowerCase();
+      if (CONTENT.indexOf(value.toLowerCase()) == -1) {
+        ELEMENT.classList.add("d-none");
+      } else {
+        ELEMENT.classList.remove("d-none");
+      }
+    }
   }
 
   retrieveProducts(): void {
